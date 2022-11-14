fb410ed4f2669af0a6ed1901ca6df53afe3f8ecd
Kirill Morozov <morozov.ks@gmail.com>
2022-11-15 00:30:20 +0300

    fix: hotfix

--- validators/users.js
+++ validators/users.js
@@ -4,7 +4,7 @@ export const celebrateBodyUser = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri(),
+    avatar: Joi.string().uri({ scheme: ['http', 'https'] }),
     email: Joi.string().required(),
     password: Joi.string().required().min(6),
   }),
